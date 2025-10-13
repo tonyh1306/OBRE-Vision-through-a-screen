683a2f6f15ec88127444b99be569363868657087
mayalaidler <mayalaidler@gmail.com>
2025-10-13 16:19:38 -0400

    added language options to recognize text sequence diagram

--- src/view/CmdLineUI.java
+++ src/view/CmdLineUI.java
@@ -4,8 +4,15 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class CmdLineUI implements UI {
+
+
     PrintStream ps = System.out;
     final Scanner in = new Scanner(System.in);
+
+
+    public void startVideoStreaming(){
+        ps.println("Phone turned on, video stream started");
+    }
 
 
 
