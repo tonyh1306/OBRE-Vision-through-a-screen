ab99c137cc2c3e4387a0e0439a099f5d347d5d45
Tony Nguyen <tqnguyen@vassar.edu>
2025-11-03 16:05:51 -0500

    Adds existing java files to android

---
+++ app/src/main/java/edu/vassar/cmpu203/OBRE/model/LLMAlgo.java
@@ -0,0 +1,9 @@
+package edu.vassar.cmpu203.OBRE.model;
+
+public class LLMAlgo{
+    String API_KEY = System.getenv("GEMINI_API");
+
+//    public String runAlgorithm() {
+//
+//    }
+}
---
+++ app/src/main/java/edu/vassar/cmpu203/OBRE/model/MediaAlgo.java
@@ -0,0 +1,6 @@
+package edu.vassar.cmpu203.OBRE.model;
+import java.util.List;
+
+public interface MediaAlgo {
+    public List<String> runAlgorithm();
+}
