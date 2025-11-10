43727d3801b5757a740bc876a05f0c4da2487352
mayalaidler <mayalaidler@gmail.com>
2025-11-10 16:55:31 -0500

    added an upload image button

--- android/app/src/main/java/edu/vassar/cmpu203/obre/MainActivity.java
+++ android/app/src/main/java/edu/vassar/cmpu203/obre/MainActivity.java
@@ -1,12 +1,18 @@
 package edu.vassar.cmpu203.obre;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import edu.vassar.cmpu203.obre.view.UploadImageFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button uploadButton = findViewById(R.id.upload_image_button);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                UploadImageFragment fragment = new UploadImageFragment();
+                fragmentTransaction.replace(R.id.main, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
         });
     }
 }
--- android/app/src/main/java/edu/vassar/cmpu203/obre/view/UploadImageFragment.java
+++ android/app/src/main/java/edu/vassar/cmpu203/obre/view/UploadImageFragment.java
@@ -3,7 +3,6 @@ package edu.vassar.cmpu203.obre.view;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -15,47 +14,22 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import java.io.IOException;
 
 import edu.vassar.cmpu203.obre.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UploadImageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UploadImageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageView imageView;
 
     public UploadImageFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UploadImageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UploadImageFragment newInstance(String param1, String param2) {
-        UploadImageFragment fragment = new UploadImageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static UploadImageFragment newInstance() {
+        return new UploadImageFragment();
     }
 
     @Override
@@ -68,6 +42,14 @@ public class UploadImageFragment extends Fragment {
                         Intent data = result.getData();
                         if (data != null && data.getData() != null) {
                             Uri imageUri = data.getData();
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                                if (imageView != null) {
+                                    imageView.setImageBitmap(bitmap);
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }
@@ -80,7 +62,8 @@ public class UploadImageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_image, container, false);
+        View view = inflater.inflate(R.layout.fragment_upload_image, container, false);
+        imageView = view.findViewById(R.id.imageView);
+        return view;
     }
 }
