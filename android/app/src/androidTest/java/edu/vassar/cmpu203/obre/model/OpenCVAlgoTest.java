f014d7347be26c1afb13f7faf57f5f19d5777d7a
Tony Nguyen <tqnguyen@vassar.edu>
2025-11-23 22:03:38 -0500

    Removes local.properties

---
+++ android/app/src/androidTest/java/edu/vassar/cmpu203/obre/model/OpenCVAlgoTest.java
@@ -0,0 +1,11 @@
+package edu.vassar.cmpu203.obre.model;
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class OpenCVAlgoTest {
+
+}
